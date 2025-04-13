8b39e7303d562e26e0017cb86993d4873f91ac10
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-04-13 22:21:42 +0530

    commented if value in decrypt

--- src/crypto/decrypt.rs
+++ src/crypto/decrypt.rs
@@ -1,5 +1,5 @@
 //! Decryption functions for the Paillier cryptosystem
-
+#![allow(unused_imports)]
 use crate::crypto::encrypt::Ciphertext;
 use crate::crypto::keygen::{PrivateKey, PublicKey};
 use num_bigint::BigUint;
@@ -46,11 +46,11 @@ pub fn decrypt(public_key: &PublicKey, private_key: &PrivateKey, ciphertext: &Ci
     if &public_key.n - &m < threshold {
         // This number is very close to n, likely a small negative that wrapped around
         let small_value = &public_key.n - &m;
-        if let Some(value) = small_value.to_u64() {
-           //rintln!("DEBUG: Found small negative value: -{}", value);
-            // In this case, we likely want the small positive value
-            return small_value;
-        }
+        // if let Some(value) = small_value.to_u64() {
+        //    //rintln!("DEBUG: Found small negative value: -{}", value);
+        //     // In this case, we likely want the small positive value
+        //     return small_value;
+        // }
     }
     
     // Fall back to the raw result if we can't determine a small value
