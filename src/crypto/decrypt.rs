2ca4d285650466a34cd35994af34cb34ac688e05
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-04-13 22:22:45 +0530

    finished the prev commit

--- src/crypto/decrypt.rs
+++ src/crypto/decrypt.rs
@@ -45,7 +45,7 @@ pub fn decrypt(public_key: &PublicKey, private_key: &PrivateKey, ciphertext: &Ci
     
     if &public_key.n - &m < threshold {
         // This number is very close to n, likely a small negative that wrapped around
-        let small_value = &public_key.n - &m;
+        //t small_value = &public_key.n - &m;
         // if let Some(value) = small_value.to_u64() {
         //    //rintln!("DEBUG: Found small negative value: -{}", value);
         //     // In this case, we likely want the small positive value
